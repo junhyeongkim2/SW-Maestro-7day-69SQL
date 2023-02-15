e26b7c4419bf818de4fb7131ff65947cd80a9695
jdg70 <jdk2531@gmail.com>
2023-02-15 11:55:10 +0900

    add DG repo

--- SY/select/오프라인:온라인 판매 데이터 통합하기.sql
+++
@@ -1,6 +0,0 @@
-SELECT date_format(SALES_DATE,"%Y-%m-%d") as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT from ONLINE_SALE
-where year(SALES_DATE)='2022' and month(SALES_DATE)='3'
-union all
-select date_format(SALES_DATE,"%Y-%m-%d") as SALES_DATE, PRODUCT_ID, NULL as USER_ID, SALES_AMOUNT from OFFLINE_SALE
-where year(SALES_DATE)='2022' and month(SALES_DATE)='3'
-order by SALES_DATE asc, PRODUCT_ID asc, USER_ID asc;
