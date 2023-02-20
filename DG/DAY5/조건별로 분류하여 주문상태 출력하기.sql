21d5e50e47278e1055397d69fe0fd171b4e42be2
jdg70 <jdk2531@gmail.com>
2023-02-20 14:23:35 +0900

    solve

---
+++ DG/DAY5/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,9 @@
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') as OUT_DATE, (
+    CASE
+        WHEN DATE_FORMAT(OUT_DATE, '%Y-%m-%d') <= '2022-05-01' THEN '출고완료'
+        WHEN OUT_DATE IS NULL THEN '출고미정'
+        ELSE '출고대기'
+    END
+) as 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
